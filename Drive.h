c59d59139231491b0f5b9bc121f7692e219d0806
CatherineHancock <17hancockc@ttsdstudents.org>
2016-02-19 17:45:42 -0800

    Update Drive.h

--- Drive.h
+++ Drive.h
@@ -1,18 +1,19 @@
-#ifndef DRIVE_H__INCLUDED
-#define DRIVE_H__INCLUDED
+#ifndef DRIVE_H__INCLUDED // checks to see if DRIVE_H__INCLUDED has been defined
+#define DRIVE_H__INCLUDED // define DRIVE_H__INCLUDED is it has not already been defined
 
 
-#include "WPILib.h"
-#include "Config.h"
+#include "WPILib.h" // include the WPILib library
+#include "Config.h" // include the Config.h header file
 
-
+// define class Drive
 class Drive{
+// variables accessible from anywhere
 public:
 	Drive(void);
 	~Drive(void);
-	
+	// SetMotors function
 	void SetMotors(float, float, float, float, bool);
-	
+// variables only accessible from other members of the same class	
 private:
 	Talon *FrontLeft, *FrontRight, *BackLeft, *BackRight;
 };
